da19a1e2c21fa1591da239fcba4a727e172c660e
NT-Van-Khanh <ntvk137@gmail.com>
2025-06-16 22:05:47 +0700

    refactor: change api

--- src/service/api.js
+++ src/service/api.js
@@ -4,8 +4,8 @@ export const fetchProducts = async () => {
       if (!response.ok) {
           throw new Error("Lỗi khi lấy dữ liệu");
       }
-      const data = await response.json();
-      return data;
+      const json = await response.json();
+      return json.data; // ✅ Trả về danh sách sản phẩm
   } catch (error) {
       console.error(error);
       return [];
@@ -18,8 +18,8 @@ export const fetchFeaturedProducts = async () => {
         if (!response.ok) {
             throw new Error("Lỗi khi lấy dữ liệu"); 
         }
-        const data = await response.json();
-        return data;
+        const json = await response.json();
+        return json.data;
     } catch (error) {
         console.error(error);
         return [];
@@ -28,13 +28,14 @@ export const fetchFeaturedProducts = async () => {
 
 export const fetchCheckStock = async (product_id) => {
     try {
-        const response = await fetch(`http://localhost:8088/api/product/check-stock/${product_id}`);
+        const response = await fetch(`http://localhost:8088/api/products/check-stock/${product_id}`);
         if (!response.ok) {
             throw new Error("Lỗi khi lấy dữ liệu từ API.");
         }
-        const maxQuantity = await response.json();
-        console.log("maxQuantity",maxQuantity);
-        return maxQuantity;
+        // const maxQuantity = await response.json();
+        // console.log("maxQuantity",maxQuantity);
+        const json = await response.json();
+        return json.data;
     } catch (error) {
         console.error(error);
         return null;
@@ -43,13 +44,13 @@ export const fetchCheckStock = async (product_id) => {
 
 export const fetchProductDetailById = async(product_id) =>{
     try{
-        const response = await fetch(`http://localhost:8088/api/product/${product_id}`);
+        const response = await fetch(`http://localhost:8088/api/products/${product_id}`);
         if (!response.ok) {
             throw new Error(`Lỗi lấy dữ liệu từ API với ID ${product_id}`);
         }
 
-        const product = await response.json();
-        return product;
+        const json = await response.json();
+        return json.data;
     } catch (error) {
         console.error("Lỗi khi tải thông tin sản phẩm", error);
         return null; // You can return null in case of an error
@@ -58,13 +59,13 @@ export const fetchProductDetailById = async(product_id) =>{
 
 export const fetchProductSimpleInfo = async(product_id) =>{
     try{
-        const response = await fetch(`http://localhost:8088/api/product/simple/${product_id}`);
+        const response = await fetch(`http://localhost:8088/api/products/simple/${product_id}`);
         if (!response.ok) {
             throw new Error(`Lỗi lấy dữ liệu từ API với ID ${product_id}`);
         }
 
-        const product = await response.json();
-        return product;
+        const json = await response.json();
+        return json.data;
     } catch (error) {
         console.error("Lỗi khi tải thông tin sản phẩm", error);
         return null; // You can return null in case of an error
